ecdf524ca5ca842bbe47ba579324adaa8a2d0e81
mrzsurendra <154421519+mrzsurendra@users.noreply.github.com>
2023-12-20 15:50:55 +0530

    Add files via upload

---
+++ sureendra.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add;
+	printf(" enter any two numbers %d%d ",a,b);
+	scanf("%d%d",&a,&b);
+	add=a+b;
+	printf("sum of two numbers %d",add);
+	return 0;
+	
+}
